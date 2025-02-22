c656c283bef7150ba77dbd5231baf41f4eb86ece
pushx-stack <rdxr7626@gmail.com>
2025-02-22 22:44:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const links = document.querySelectorAll("nav ul li a");
+
+    links.forEach(link => {
+        link.addEventListener("click", function (e) {
+            e.preventDefault();
+            const targetId = this.getAttribute("href").substring(1);
+            const targetSection = document.getElementById(targetId);
+
+            if (targetSection) {
+                window.scrollTo({
+                    top: targetSection.offsetTop - 50,
+                    behavior: "smooth"
+                });
+            }
+        });
+    });
+});
+
